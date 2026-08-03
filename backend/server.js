a622a5d222457b16ec5b406142d91dda8c166030
lucifercode17 <jareehd121@gmail.com>
2026-08-03 22:39:17 +0530

    cors change for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(cors({
   origin:[ "http://localhost:5173",
-    "https://real-estate-ai-agent-tdt2.onrender.com"
+    "https://real-estate-ai-agent-blue.vercel.app"
   ],
   credentials: true}));
 
