daea3231ced669641d0a4d8740230162305b237f
lucifercode17 <jareehd121@gmail.com>
2026-08-04 13:11:31 +0530

    vercal app link changes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(cors({
   origin:[ "http://localhost:5173",
-    "https://real-estate-ai-agent-weld.vercel.app/"
+    "https://real-estate-ai-agent-weld.vercel.app"
   ],
   credentials: true}));
 
